cb90a489a57c0484c2a0a4a000d155ab0d913fe2
msk- <mattkingston@gmail.com>
2021-05-11 16:05:51 +0100

    Keyboard update

--- keyboard/moonlander_msk_source/keymap.c
+++ keyboard/moonlander_msk_source/keymap.c
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LEFT,                                        KC_RIGHT,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     KC_GRAVE,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(1),                                          TG(1),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_NONUS_HASH,  
-    KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_HYPR,                                                                        KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           LT(2,KC_SCOLON),LGUI_T(KC_QUOTE),
-    LSFT_T(KC_NONUS_BSLASH),LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(KC_SLASH),KC_RSHIFT,      
+    KC_NONUS_BSLASH,KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_HYPR,                                                                        KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           LT(2,KC_SCOLON),LGUI_T(KC_QUOTE),
+    KC_LSHIFT,      LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(KC_SLASH),KC_RSHIFT,      
     TD(DANCE_0),    WEBUSB_PAIR,    LALT(KC_LSHIFT),KC_HOME,        KC_LALT,        LALT_T(KC_APPLICATION),                                                                                                LCTL_T(KC_ESCAPE),KC_LALT,        KC_END,         KC_LBRACKET,    KC_RBRACKET,    MO(1),          
     KC_SPACE,       KC_BSPACE,      KC_LGUI,                        LT(1,KC_NO),    KC_TAB,         KC_ENTER
   ),
