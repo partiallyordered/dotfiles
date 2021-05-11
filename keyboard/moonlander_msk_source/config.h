e7425c5aab086ed30897dc75d8bb63ce3d507408
msk- <mattkingston@gmail.com>
2021-05-11 14:51:34 +0100

    Add keyboard firmware

---
+++ keyboard/moonlander_msk_source/config.h
@@ -0,0 +1,8 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"4DBEQ/47OmM"
+#define RGB_MATRIX_STARTUP_SPD 60
