fcabb5165036b9a65760432951f7d8be01587f3f
Matt Kingston <mattkingston@protonmail.com>
2022-05-02 20:50:48 +0200

    Fix close behavior in wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,7 @@ return {
     enable_tab_bar = false,
     font_size = 20.0,
     font = wezterm.font("Fira Code"),
-    exit_behaviour = "Close",
+    exit_behavior = "Close",
     -- color_scheme = "Solarized Dark",
     color_scheme = "Spacedust",
     window_background_opacity = 0.9,
