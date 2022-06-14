62dd66a39c260d597dde0deda2bb6b7ed18be2a1
Matt Kingston <mattkingston@protonmail.com>
2022-06-14 12:44:06 +0200

    Create a 'sys' registry to use with nix search

--- flake.nix
+++ flake.nix
@@ -23,6 +23,22 @@
           # Follows
           # https://github.com/nix-community/NUR/tree/287aa9ab138e00c3a3519e437a56b7627c62c478#flake-support
           { nixpkgs.overlays = [ nur.overlay ]; }
+
+          # https://discourse.nixos.org/t/local-flake-based-nix-search-nix-run-and-nix-shell/13433/6
+          { nix.nixPath = [ "nixpkgs=${nixpkgs}" ]; }
+
+          # Create a "sys" registry aliasing the nixpkgs flake here to use with `nix search sys`
+          # https://discourse.nixos.org/t/local-flake-based-nix-search-nix-run-and-nix-shell/13433/4
+          ({ pkgs, ... }: {
+            nix.registry.sys = {
+              from = { type = "indirect"; id = "sys"; };
+              flake = nixpkgs;
+            };
+            # We could also have nixpkgs pinned to the system version like so:
+            # https://gitlab.com/azazel/ender-config/-/blob/6771570f45e0c68b9647f4c4e52dc797d2d6267d/flake.nix#L51-54
+            # nix.registry.nixpkgs.flake = nixpkgs;
+          })
+
           ./configuration.nix
           home-manager.nixosModules.home-manager
           {
