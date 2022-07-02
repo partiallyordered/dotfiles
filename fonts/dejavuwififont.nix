e2b92b847c5a5f93b368c8e91a4d517530393700
msk- <mattkingston@gmail.com>
2022-07-02 18:46:47 +0200

    Fix fonts

--- fonts/dejavuwififont.nix
+++ fonts/dejavuwififont.nix
@@ -1,23 +1,25 @@
-{ lib, fetchzip }:
+{ pkgs, lib }:
 
-let
-  version = "c88c3a6bf0656e915456688d5a8e938fcc5c5d68";
-in fetchzip {
-  name = "dejavu-wifi-icons-${version}";
-
-  url = "https://github.com/isaif/polybar-wifi-ramp-icons/archive/${version}.zip";
-
-  postFetch = ''
-    mkdir -p $out/share/fonts
-    unzip -j $downloadedFile '*.ttf' -d $out/share/fonts/truetype
-  '';
-
-  sha256 = "m0IdymWFUWUXp58jCQh+b28evywFxwdybpm4pTaWUJA=";
-
+pkgs.stdenv.mkDerivation rec {
   meta = with lib; {
     homepage = "https://github.com/isaif/polybar-wifi-ramp-icons";
     description = "A patched \"DejaVu Sans Mono\" font that includes the wifi icons to be used in polybar network ramp-signal";
     license = licenses.mit;
     platforms = platforms.all;
   };
+  version = "c88c3a6bf0656e915456688d5a8e938fcc5c5d68";
+  pname = "dejavu-wifi-icons";
+  src = builtins.fetchurl {
+    url = "https://github.com/isaif/polybar-wifi-ramp-icons/raw/${version}/DejaVuSansMono-wifi-ramp.ttf";
+    sha256 = "1nq82zcrxp95qcchj2ig998dg63vf0v7pwgqvgv2fnjc3mn4vb5g";
+  };
+  dontUnpack = true;
+  # installPhase = ''
+  #   mkdir -p $out/share/fonts/truetype
+  #   find -type f -iname '*.ttf' -exec mv {} $out/share/fonts/truetype/ \;
+  #   '';
+  installPhase = ''
+    mkdir -p $out/share/fonts/truetype
+    cp $src $out/share/fonts/truetype/
+    '';
 }
