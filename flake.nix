52c003d341ba8e6cfc88e3e6ddd019d123a0e4f0
Matt Kingston <mattkingston@protonmail.com>
2022-07-03 19:16:23 +0100

    Move zen modules to shared modules

--- flake.nix
+++ flake.nix
@@ -16,13 +16,9 @@
   };
 
   outputs = { home-manager, nixpkgs, nur, ... }: {
-    nixosConfigurations = {
-      zen = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          # Hardware
-          ./zen-hw-conf.nix
-
+    nixosConfigurations =
+      let
+        sharedModules = [
           # Follows
           # https://github.com/nix-community/NUR/tree/287aa9ab138e00c3a3519e437a56b7627c62c478#flake-support
           { nixpkgs.overlays = [ nur.overlay ]; }
@@ -53,44 +49,52 @@
             # arguments to home.nix
           }
         ];
-      };
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          # Hardware
-          ./xps-hw-conf.nix
+      in
+        {
+          zen = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              # Hardware
+              ./zen-hw-conf.nix
+            ] ++ sharedModules;
+          };
+          nixos = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              # Hardware
+              ./xps-hw-conf.nix
 
-          # Follows
-          # https://github.com/nix-community/NUR/tree/287aa9ab138e00c3a3519e437a56b7627c62c478#flake-support
-          { nixpkgs.overlays = [ nur.overlay ]; }
+              # Follows
+              # https://github.com/nix-community/NUR/tree/287aa9ab138e00c3a3519e437a56b7627c62c478#flake-support
+              { nixpkgs.overlays = [ nur.overlay ]; }
 
-          # https://discourse.nixos.org/t/local-flake-based-nix-search-nix-run-and-nix-shell/13433/6
-          { nix.nixPath = [ "nixpkgs=${nixpkgs}" ]; }
+              # https://discourse.nixos.org/t/local-flake-based-nix-search-nix-run-and-nix-shell/13433/6
+              { nix.nixPath = [ "nixpkgs=${nixpkgs}" ]; }
 
-          # Create a "sys" registry aliasing the nixpkgs flake here to use with `nix search sys`
-          # https://discourse.nixos.org/t/local-flake-based-nix-search-nix-run-and-nix-shell/13433/4
-          ({ pkgs, ... }: {
-            nix.registry.sys = {
-              from = { type = "indirect"; id = "sys"; };
-              flake = nixpkgs;
-            };
-            # We could also have nixpkgs pinned to the system version like so:
-            # https://gitlab.com/azazel/ender-config/-/blob/6771570f45e0c68b9647f4c4e52dc797d2d6267d/flake.nix#L51-54
-            # nix.registry.nixpkgs.flake = nixpkgs;
-          })
+              # Create a "sys" registry aliasing the nixpkgs flake here to use with `nix search sys`
+              # https://discourse.nixos.org/t/local-flake-based-nix-search-nix-run-and-nix-shell/13433/4
+              ({ pkgs, ... }: {
+                nix.registry.sys = {
+                  from = { type = "indirect"; id = "sys"; };
+                  flake = nixpkgs;
+                };
+                # We could also have nixpkgs pinned to the system version like so:
+                # https://gitlab.com/azazel/ender-config/-/blob/6771570f45e0c68b9647f4c4e52dc797d2d6267d/flake.nix#L51-54
+                # nix.registry.nixpkgs.flake = nixpkgs;
+              })
 
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.msk = import ./home.nix;
+              ./configuration.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.msk = import ./home.nix;
 
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
-      };
-    };
+                # Optionally, use home-manager.extraSpecialArgs to pass
+                # arguments to home.nix
+              }
+            ];
+          };
+        };
   };
 }
