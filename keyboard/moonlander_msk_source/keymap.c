fea7780d63a291456f1e2279e2d8dce3e14b7f19
Matt Kingston <mattkingston@protonmail.com>
2022-11-24 20:57:14 +0000

    Keyboard firmware update

--- keyboard/moonlander_msk_source/keymap.c
+++ keyboard/moonlander_msk_source/keymap.c
@@ -57,11 +57,11 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
-    KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LEFT,                                        KC_RIGHT,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
-    KC_GRAVE,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(1),                                          TG(1),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_NONUS_HASH,  
-    KC_NONUS_BSLASH,KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_HYPR,                                                                        KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           LT(2,KC_SCOLON),MT(MOD_LGUI, KC_QUOTE),
+    UK_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LEFT,                                        KC_RIGHT,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(1),                                          TG(1),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_NONUS_HASH,  
+    KC_NONUS_BSLASH,KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_HYPR,                                                                        UK_EQL,         KC_H,           KC_J,           KC_K,           KC_L,           LT(2,KC_SCOLON),KC_QUOTE,       
     KC_LSHIFT,      MT(MOD_LCTL, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         MT(MOD_RCTL, KC_SLASH),KC_RSHIFT,      
-    MT(MOD_LGUI, KC_DELETE),WEBUSB_PAIR,    LALT(KC_LSHIFT),KC_HOME,        KC_LALT,        MT(MOD_LALT, KC_APPLICATION),                                                                                                MT(MOD_LCTL, KC_ESCAPE),KC_LALT,        KC_END,         KC_LBRACKET,    KC_RBRACKET,    MO(1),          
+    KC_DELETE,      WEBUSB_PAIR,    CAPS_WORD,      KC_HOME,        KC_LALT,        MT(MOD_LALT, KC_APPLICATION),                                                                                                MT(MOD_LCTL, KC_ESCAPE),KC_LALT,        KC_END,         KC_LBRACKET,    KC_RBRACKET,    UK_EQL,         
     KC_SPACE,       KC_BSPACE,      MO(1),                          MO(1),          KC_TAB,         KC_ENTER
   ),
   [1] = LAYOUT_moonlander(
